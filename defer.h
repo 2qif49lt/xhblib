96915bd44258d9637eaeb8445a4808a368bfe818
downmooner <11she_232@163.com>
2013-01-04 18:23:40 +0800

    add macro
    add macro

--- defer.h
+++ defer.h
@@ -2,27 +2,29 @@
 #define DEFER_LANGUAGE_XHB_H_
 
  /*
- void testdefer()
+  void testdefer()
  {
      FILE* pf = fopen("d:/1.txt","r");
-     defer filehandle([&](){if(pf)fclose(pf);});
+     defer([&](){if(pf)fclose(pf);});
      
      char chbuff[10240] = {};
-     
-     int iread = fread(chbuff,sizeof(char),sizeof(chbuff),pf);
-     if(iread > 0)
+     if(pf)
+     {
+         int iread = fread(chbuff,sizeof(char),sizeof(chbuff),pf);
+        if(iread > 0)
          cout<<chbuff<<endl;
-
+     }
  }
  */
  #include<functional>
 namespace xhb
 {
- class defer
+ 
+ class defer_
  {
  public:
-     explicit defer(std::function<void()> onexit) : m_onexit(onexit),m_bdismiss(false){}
-     ~defer()
+     explicit defer_(std::function<void()> onexit) : m_onexit(onexit),m_bdismiss(false){}
+     ~defer_()
      {
          if(!m_bdismiss)
              m_onexit();
@@ -33,9 +35,13 @@ namespace xhb
      bool m_bdismiss;
  
  private:
-     defer(const defer& rhs);
-     defer& operator= (const defer& rhs);
+     defer_(const defer_& rhs);
+     defer_& operator= (const defer_& rhs);
  };
+ 
+#define defer_name_cat(name,line) name##line
+#define defer_name(name,line) defer_name_cat(name,line)
+#define defer(fn) defer_ defer_name(defer_name_,__LINE__) (fn)
 
  }
 #endif //DEFER_LANGUAGE_XHB_H_
